0caf672f6723d9fda5cd640f80fb18ad1e263712
beixinci <beixinci@outlook.com>
2024-02-19 17:29:42 +0800

    fresh

---
+++ manage.h
@@ -0,0 +1,25 @@
+#ifndef MANAGE_H
+#define MANAGE_H
+
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include "md5.h"
+#include "sha.h"
+#include "mem_list.h"
+#include "user_mgr.h"
+
+
+void init_gl(void);
+void user_menu(void);
+void mem_menu(void);
+void delete_mem(void);
+void update_mem(void);
+int show_mem(struct user_list* g);
+void look_myself(void);
+void search_mem(void);
+void update_mem1(void);
+
+extern int member_id;
+
+#endif
